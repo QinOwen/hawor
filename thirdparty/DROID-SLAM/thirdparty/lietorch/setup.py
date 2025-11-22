6f7f041841bbd0bf039fc813096919a217ef0bd3
QinOwen <110217995+QinOwen@users.noreply.github.com>
2025-11-22 05:24:53 +0000

    change droid-slam source code to support new pytorch

---
+++ thirdparty/DROID-SLAM/thirdparty/lietorch/setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+import os.path as osp
+
+
+ROOT = osp.dirname(osp.abspath(__file__))
+
+setup(
+    name='lietorch',
+    version='0.2',
+    description='Lie Groups for PyTorch',
+    author='teedrz',
+    packages=['lietorch'],
+    ext_modules=[
+        CUDAExtension('lietorch_backends', 
+            include_dirs=[
+                osp.join(ROOT, 'lietorch/include'), 
+                osp.join(ROOT, 'eigen')],
+            sources=[
+                'lietorch/src/lietorch.cpp', 
+                'lietorch/src/lietorch_gpu.cu',
+                'lietorch/src/lietorch_cpu.cpp'],
+            extra_compile_args={
+                'cxx': ['-O2'], 
+                'nvcc': ['-O2',
+                    '-gencode=arch=compute_60,code=sm_60', 
+                    '-gencode=arch=compute_61,code=sm_61', 
+                    '-gencode=arch=compute_70,code=sm_70', 
+                    '-gencode=arch=compute_75,code=sm_75',
+                    '-gencode=arch=compute_75,code=compute_75',
+                    '-gencode=arch=compute_90,code=sm_90',
+                    '-gencode=arch=compute_90,code=compute_90',
+                    '-gencode=arch=compute_100,code=sm_100',
+                    '-gencode=arch=compute_100,code=compute_100',
+                ]
+            }),
+
+        CUDAExtension('lietorch_extras', 
+            sources=[
+                'lietorch/extras/altcorr_kernel.cu',
+                'lietorch/extras/corr_index_kernel.cu',
+                'lietorch/extras/se3_builder.cu',
+                'lietorch/extras/se3_inplace_builder.cu',
+                'lietorch/extras/se3_solver.cu',
+                'lietorch/extras/extras.cpp',
+            ],
+            extra_compile_args={
+                'cxx': ['-O2'], 
+                'nvcc': ['-O2',
+                    '-gencode=arch=compute_60,code=sm_60', 
+                    '-gencode=arch=compute_61,code=sm_61', 
+                    '-gencode=arch=compute_70,code=sm_70', 
+                    '-gencode=arch=compute_75,code=sm_75',
+                    '-gencode=arch=compute_75,code=compute_75',
+                    '-gencode=arch=compute_90,code=sm_90',
+                    '-gencode=arch=compute_90,code=compute_90',
+                    '-gencode=arch=compute_100,code=sm_100',
+                    '-gencode=arch=compute_100,code=compute_100',
+                    
+                ]
+            }),
+    ],
+    cmdclass={ 'build_ext': BuildExtension }
+)
+
+
